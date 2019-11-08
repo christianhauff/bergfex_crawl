9123a3c601949da83dd37bde613a2da30c88559a
Christian Hauff <24371132+christianhauff@users.noreply.github.com>
2019-11-08 11:53:56 +0100

    fixed script to work w/ latest bergfex html layout

--- main.py
+++ main.py
@@ -1,46 +1,42 @@
-#imports
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime, timedelta
 
-#function definition for getting pages and parsing content
-def myFunc(url):
+def getWeather(url):
     #get page content via url
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     #parsing
-    weatherArray = soup.find_all("div", {"day clickable selectable fields"})
-    i = 0
+    weatherArray = soup.select("div.day.fields")
+    result = []
     for day in weatherArray:
-        myDate = datetime.now().date() + timedelta(days=i)
+        dayresult = {}
+        
+        dayresult['tmax'] = int(day.find("div", {"class":"tmax"}).text.replace("°C",""))
+        dayresult['tmin'] = int(day.find("div", {"class":"tmin"}).text.replace("°C",""))
+        dayresult['freeText'] = day.find("div", {"class":"icon"}).find("img")['title']
+        dayresult['neuschnee'] = day.find("div", {"class":"nschnee"}).text.replace("\n","").replace("cm","")
+        
+        if (dayresult['neuschnee'] == "-"):
+            dayresult['neuschnee'] = 0
+            
+        if (dayresult['neuschnee'] == "<1"):
+            dayresult['neuschnee'] = 1
+            
+        if (isinstance(dayresult['neuschnee'],str)):
+            if (dayresult['neuschnee'].isnumeric()):
+                dayresult['neuschnee'] = int(dayresult['neuschnee'])
+        
+        result.append(dayresult)
+    return result
 
-        tmax = day.find("div", {"class":"tmax"}).text
-        tmin = day.find("div", {"class":"tmin"}).text
-        freeText = day.find("div", {"class":"icon"}).find("img")['title']
-
-        try:
-            snow = day.find("div", {"class":"group nschnee important"}).text.replace("\n","").replace("\t","").replace(" ","")
-        except:
-            snow = day.find("div", {"class":"group nschnee "}).text.replace("\n","").replace("\t","").replace(" ","")
-
-        print(str(myDate) + " weekday: " + str(myDate.weekday()) + " - " + "Neuschnee: " + snow + ", " + str(freeText) + " bei " + tmin + " bis " + tmax)
-        i += 1
-
-#testurl
-kitzUrl = "http://www.bergfex.at/kitzsteinhorn-kaprun/wetter/berg/"
-
-#main url array
-urls= [
-"http://www.bergfex.at/kitzsteinhorn-kaprun/wetter/berg/",
-"http://www.bergfex.at/obertauern/wetter/berg/",
-"http://www.bergfex.at/zell-am-ziller/wetter/berg/",
-"http://www.bergfex.at/zauchensee/wetter/berg/",
-"http://www.bergfex.at/flachau-wagrain-alpendorf/wetter/berg/"
-]
-
-#main program flow
-print(" ")
-for url in urls:
-    print(url.split('/')[3])
-    myFunc(url)
-    print(" ")
+# > getWeather(url)
+# [{'tmax': 3, 'tmin': 1, 'freeText': 'Schneeregen 0h Sonne', 'neuschnee': 3},
+#  {'tmax': 1, 'tmin': 0, 'freeText': 'bedeckt 0h Sonne', 'neuschnee': 0},
+#  {'tmax': 4, 'tmin': 2, 'freeText': 'wolkenlos 8h Sonne', 'neuschnee': 0},
+#  {'tmax': 5, 'tmin': 0, 'freeText': 'wolkenlos 8h Sonne', 'neuschnee': 0},
+#  {'tmax': 2, 'tmin': -1, 'freeText': 'wolkig 3h Sonne', 'neuschnee': 0},
+#  {'tmax': 1, 'tmin': -2, 'freeText': 'wolkig 3h Sonne', 'neuschnee': 0},
+#  {'tmax': 0, 'tmin': -3, 'freeText': 'stark bewölkt 1h Sonne', 'neuschnee': 1},
+#  {'tmax': 0, 'tmin': -3, 'freeText': 'bedeckt 0h Sonne', 'neuschnee': 0},
+#  {'tmax': 3, 'tmin': -3, 'freeText': 'wolkig 2h Sonne', 'neuschnee': 0}]
